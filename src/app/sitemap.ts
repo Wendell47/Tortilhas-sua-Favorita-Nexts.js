683bfe1811250da4849905474a28fb247fd9df89
Wendell47 <ps4.wendell@gmail.com>
2023-05-26 10:02:35 -0400

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,11 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: '/pages/about',
+      url: 'https://tortilhas-sua-favorita-nexts-js.vercel.app/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://tortilhas-sua-favorita-nexts-js.vercel.app/pages/about',
       lastModified: new Date(),
     },
   ];
